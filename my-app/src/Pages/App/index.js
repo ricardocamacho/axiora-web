72c6c3a33420e0f6407d78facc1343771d6bb170
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-04-22 22:09:42 -0500

    Create React App and Tailwind styles

---
+++ my-app/src/Pages/App/index.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className="text-3xl flex justify-center bg-red-50 underline">Todo inicia</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
