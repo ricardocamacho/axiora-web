80b84b85f16f27b4c71e4b772c288cd1e0eaad59
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-05-10 20:51:59 -0500

    organizando archivos

---
+++ src/app/api/api.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const baseUrl = {
+  development: 'https://bcq3petmyf.execute-api.us-east-2.amazonaws.com/dev',
+  production: 'https://ssa61tzph4.execute-api.us-east-2.amazonaws.com/prod'
+};
+
+// Singleton instance
+let instance = null;
+
+class Api {
+  constructor() {
+    if (!instance) {
+      this.axioraApi = axios.create({
+        baseURL: baseUrl[process.env.REACT_APP_ENV],
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      instance = this;
+    }
+    return instance;
+  }
+
+  setToken(token) {
+    this.axioraApi.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
+
+  async signUp(email, password) {
+    const response = await this.axioraApi.post('/sign-up', { email, password });
+    return response.data;
+  }
+
+  async loginUser(email, password) {
+    const response = await this.axioraApi.post('/sign-in', { email, password });
+    return response.data;
+  }
+
+  async getStores() {
+    const response = await this.axioraApi.get('/stores');
+    return response.data;
+  }
+
+  async addStore(meliUserId, code, redirectUri) {
+    const response = await this.axioraApi.post('/mercadolibre/store', {
+      meliUserId,
+      code,
+      redirectUri
+    });
+    return response.data;
+  }
+
+  async updateInventory(sku, quantity) {
+    const response = await this.axioraApi.put('/inventory', { sku, quantity });
+    return response.data;
+  }
+}
+
+export default Api;
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,37 @@
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//     return (
+//         <main>
+//             { children }
+//         </main>
+//     )
+// }
 
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <main>
-            { children }
-        </main>
-    )
-}
+import TopNav from '@/containers/Topnav'
+import Sidebar from '@/containers/Sidebar'
+import Footer from '@/containers/Footer'
+import Link from 'next/link';
+
+const AppLayout = ({ containerClassnames, children, history }) => {
+  return (
+    <div id="app-container" className={containerClassnames}>
+      <TopNav history={history} />
+      <Sidebar />
+      <main>
+        <div className="container-fluid">{children}</div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+const mapStateToProps = ({ menu }) => {
+  const { containerClassnames } = menu;
+  return { containerClassnames };
+};
+const mapActionToProps = {};
+
+export default Link(
+  connect(mapStateToProps, mapActionToProps)(AppLayout)
+);
